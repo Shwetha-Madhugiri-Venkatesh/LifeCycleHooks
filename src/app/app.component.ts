ca252102f5f6eecaa0d242cfd9bd04136ecd2a39
Shwetha <Shwetha.Venkatesh@itorizon.com>
2025-06-04 00:22:43 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LifeCycleHooks';
+  inputMsg:string='';
+
+  inputMsg1:string='';
+
+  normal:string='Bleven';
+  Destroy:boolean=true;
+  constructor(){
+    console.log("App Component is executing!");
+  }
+
+  assignInputMsg(inpEl:HTMLInputElement){
+    this.inputMsg=inpEl.value;
+  }
+   assignInputMsg1(inpEl:HTMLInputElement){
+    this.inputMsg1=inpEl.value;
+  }
+
+  focus(){
+    console.log("hi");
+    this.fuc();
+  }
+
+  fuc(){
+    this.normal="Nick";
+  }
+  ngAfterViewInit(){
+    console.log("AppComponent hook");
+  }
+
+  DestroyComp(){
+    this.Destroy=!this.Destroy;
+  }
 }
---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,68 @@
+import { Component, ContentChild, DoCheck, ElementRef, Input, OnChanges, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.css']
+})
+export class DemoComponent implements OnChanges,OnInit,DoCheck{
+  normal_var="this is normal variable";
+  // @Input() msg="hi";
+  @Input() msg:string='';
+
+  @Input() msg1:string='';
+  @ViewChild('para') para:ElementRef;
+  constructor(){
+    console.log("Demo component is executing");
+    console.log(this.normal_var);
+    console.log(this.msg);
+  }
+  new_msg="ammu";
+  @ContentChild('content') con:ElementRef;
+  ngOnChanges(){
+    console.log("ngOnchanges hook is called!");
+    console.log(this.msg);
+    // this.msg=this.new_msg;
+    //this is executed before the html is rendered
+  }
+
+  ngOnInit(){
+    console.log("ngOnInit is called!");
+    // console.log(this.con.nativeElement);
+  }
+
+  ngDoCheck(){
+    console.log("ngDoCheck is called");
+    //console.log(this.con.nativeElement.innerHTML);
+    if(this.con){
+       console.log(this.con.nativeElement.innerHTML);
+    }else{
+      console.log("not initialized");
+    }
+  }
+
+  ngAfterContentInit(){
+    console.log("ngAfterContentInit is called");
+    console.log(this.con.nativeElement);
+  }
+
+  ngAfterContentChecked(){
+    console.log("ngAfterContentChecked is called");
+     console.log(this.con.nativeElement.innerHTML);
+     console.log(this.para);
+  }
+
+  ngAfterViewInit(){
+     console.log("ngAfterViewInit is called");
+    console.log(this.para.nativeElement);
+  }
+
+  ngAfterViewChecked(){
+    console.log("ngAfterViewChecked is called");
+    console.log(this.para.nativeElement);
+  }
+
+  ngOnDestroy(){
+   console.log("ngOnDestroy is called");
+  }
+}
